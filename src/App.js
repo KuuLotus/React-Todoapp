e03ae31c027541c36780b654c2c43b112966a789
KuuLotus <kuulotusy@gmail.com>
2022-08-16 21:59:48 +0900

    [finish] todoapp

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
-import logo from "./logo.svg";
+import { useState } from "react";
 import "./App.css";
+import InputForm from "./components/InputForm";
+import Title from "./components/Title";
+import TodoList from "./components/TodoList";
 
 function App() {
-  return <div className="App"></div>;
+  const [taskList, setTaskList] = useState([]);
+  return (
+    <div className="body">
+      <Title />
+      <InputForm taskList={taskList} setTaskList={setTaskList} />
+      <TodoList taskList={taskList} setTaskList={setTaskList} />
+    </div>
+  );
 }
 
 export default App;
